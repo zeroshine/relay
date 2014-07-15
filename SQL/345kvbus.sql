1a38c129c75060e86fdb67e09c290c763f321c24
zeroshine <as790726@gmail.com>
2014-07-15 16:27:33 +0800

    after train and clean

---
+++ SQL/345kvbus.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- 主機: localhost
+-- 產生日期: 2014 年 07 月 10 日 23:34
+-- 伺服器版本: 5.6.12-log
+-- PHP 版本: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 資料庫: `relay`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的結構 `345kvbus`
+--
+
+CREATE TABLE IF NOT EXISTS `345kvbus` (
+  `name` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `break` tinyint(4) NOT NULL,
+  `relayset` tinyint(4) NOT NULL,
+  `short_3` float NOT NULL,
+  `ground_1` float NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 轉存資料表中的資料 `345kvbus`
+--
+
+INSERT INTO `345kvbus` (`name`, `break`, `relayset`, `short_3`, `ground_1`) VALUES
+('B中火#10', 1, 0, 0, 0),
+('B中火#9', 1, 0, 0, 0),
+('中寮北E', 1, 0, 0, 0),
+('中寮南E', 1, 0, 0, 0),
+('中港E', 1, 0, 0, 0),
+('中火北E', 1, 0, 0, 0),
+('中火南E', 1, 0, 0, 0),
+('中科E', 1, 0, 0, 0),
+('五甲E', 1, 0, 0, 0),
+('仁武E', 1, 0, 0, 0),
+('仙渡E', 1, 0, 0, 0),
+('全興E', 1, 0, 0, 0),
+('冬山E', 1, 0, 0, 0),
+('協和E', 1, 0, 0, 0),
+('南投E', 1, 0, 0, 0),
+('南科E', 1, 0, 0, 0),
+('后里E', 1, 0, 0, 0),
+('和平E', 1, 0, 0, 0),
+('嘉民E', 1, 0, 0, 0),
+('國光E', 1, 0, 0, 0),
+('塑化E', 1, 0, 0, 0),
+('大潭E', 1, 0, 0, 0),
+('大觀二E', 1, 0, 0, 0),
+('大鵬E', 1, 0, 0, 0),
+('天輪E', 1, 0, 0, 0),
+('峨眉E', 1, 0, 0, 0),
+('彰林E', 1, 0, 0, 0),
+('彰濱E', 1, 0, 0, 0),
+('明潭E', 1, 0, 0, 0),
+('板橋E', 1, 0, 0, 0),
+('核一E', 1, 0, 0, 0),
+('核三E', 1, 0, 0, 0),
+('核二E', 1, 0, 0, 0),
+('汐止E', 1, 0, 0, 0),
+('深美E', 1, 0, 0, 0),
+('瀰力E', 1, 0, 0, 0),
+('竹工E', 1, 0, 0, 0),
+('興益E', 1, 0, 0, 0),
+('興達北E', 1, 0, 0, 0),
+('興達南E', 1, 0, 0, 0),
+('路北E', 1, 0, 0, 0),
+('霧峰#1', 1, 0, 0, 0),
+('霧峰#2', 1, 0, 0, 0),
+('霧峰#3', 1, 0, 0, 0),
+('霧峰#4', 1, 0, 0, 0),
+('霧峰E', 1, 0, 0, 0),
+('頂湖E', 1, 0, 0, 0),
+('高港E', 1, 0, 0, 0),
+('高雄E', 1, 0, 0, 0),
+('鳳林E', 1, 0, 0, 0),
+('麥寮E', 1, 0, 0, 0),
+('龍崎北E', 1, 0, 0, 0),
+('龍崎南E', 1, 0, 0, 0),
+('龍潭北E', 1, 0, 0, 0),
+('龍潭南E', 1, 0, 0, 0),
+('龍門E', 1, 0, 0, 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- psse/psse.py
+++ psse/psse.py
@@ -4,6 +4,10 @@ sys.path.append(r"C:\Program Files (x86)\PTI\PSSE32\PSSBIN")
 os.environ['PATH'] = (r"C:\Program Files (x86)\PTI\PSSE32\PSSBIN;"
                       + os.environ['PATH'])
 
+#add the the above line 
+
+
+#Here is the macro script
 import psspy
 import redirect
 _i=psspy.getdefaultint()
@@ -12,8 +16,8 @@ _s=psspy.getdefaultchar()
 redirect.psse2py()
 import pssdb
 psspy.psseinit(80000)
-psspy.case(r"""10207Pm-32v.sav""")
-psspy.resq(r"""10207Pm.seq""")
+psspy.case(r"""psse.sav""")
+psspy.resq(r""" psse.seq""")
 psspy.lines_per_page_one_device(1,60)
 psspy.report_output(2,r"""report.txt""",[0,0])
 psspy.flat([1,1,1,0],[0.0,0.0])
@@ -23,3 +27,9 @@ psspy.scmu(1,[0,0,0,0,0,0,0],[0.0,0.0,0.0,0.0,0.0],"")
 psspy.scmu(2,[7,1082,0,0,0,0,0],[0.0,0.0,0.0,0.0,0.0],"")
 psspy.scmu(3,[7,1082,0,0,0,0,0],[0.0,0.0,0.0,0.0,0.0],"")
 psspy.sequence_network_setup(0)
+#end of script
+
+# add below line to show the report
+f=open("report.txt",'r')
+for line in f.readlines():
+	print line
